4c3e1f0ae12b3e8e8ca3b7bf1ce0e9bf9e2fc084
Bruno Pedroso <Bruno@macasa.local>
2010-05-13 11:03:38 -0300

    refatorei: extraí o método swap

--- lib/quicksorter.rb
+++ lib/quicksorter.rb
@@ -3,12 +3,16 @@ class QuickSorter
 	def self.sort(list)
 		unless (list.size < 2)
 			if (list[0] > list[1])
-				swapped = list[0]
-				list[0] = list[1]
-				list[1] = swapped
+				swap(list,0,1)
 			end
 		end
 		list
+	end
+
+	def self.swap(list, i, j)
+		swapped = list[i]
+		list[i] = list[j]
+		list[j] = swapped
 	end
 	
 end
